d8fe2c1d131acf955e200eea71143166b24b3ef5
Kiezmi Quinza <Kiezmi@gmail.com>
2018-11-26 12:44:31 +0100

    Ejercicios 1 y 2

---
+++ count-to-6/program.js
@@ -0,0 +1,12 @@
+///////Ejercico 1 y 2////////
+
+/* var array = (process.argv[2]);
+
+console.log("Hello, " + array + "!");
+console.log("Your name lowercased is " + "\"" +  array.toLowerCase()+ "\"." );
+
+ Resultado bueno y oficial.
+console.log(`Hello, ${array}!
+Your name lowercased is "${array.toLowerCase()}".`);
+ */
+
