24b92f591425653a7cdbeaa7bc458279465936d7
Kiezmi Quinza <Kiezmi@gmail.com>
2018-11-26 12:44:50 +0100

    Ejercicio 3

--- count-to-6/program.js
+++ count-to-6/program.js
@@ -10,3 +10,14 @@ console.log(`Hello, ${array}!
 Your name lowercased is "${array.toLowerCase()}".`);
  */
 
+///////Ejercico 3////////
+
+// var inputs = process.argv.slice(2);
+// var result = inputs.map(/* what goes here? */)
+//                     .reduce(/* what goes here? */);
+var array = (process.argv[2])
+var inputs = process.argv.slice(2);
+var result = inputs.map((x)=>{input.length});
+
+
+console.log(result);
