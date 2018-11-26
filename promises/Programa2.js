260189bff2624f0425ded98d0975614997e86a28
Kiezmi Quinza <Kiezmi@gmail.com>
2018-11-26 13:20:27 +0100

    Ejercicio2

---
+++ promises/Programa2.js
@@ -0,0 +1,29 @@
+require('es6-promise');
+
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(fulfill,300,"FULFILLED!");
+});
+  
+ promise.then(console.log);
+
+///////////////Solucion//////////////////////
+
+/*  'use strict';
+
+ // Create a promise
+
+ var promise = new Promise(function (fulfill, reject) {
+   // After the timeout reaches 300ms, fulfill the promise with value
+   // 'FULFILLED!'.
+
+   setTimeout(function () {
+     fulfill('FULFILLED!');
+   }, 300);
+ });
+
+ // Add a handler to the promise’s fulfillment. `console.log` will be called
+ // with the value passed to `fulfill`, which is `'FULFILLED!'`.
+ // Note that this statement will ALWAYS be executed before `fulfill` is
+ // called (we'll talk about this in depth in the lessons to come).
+
+ promise.then(console.log); */
