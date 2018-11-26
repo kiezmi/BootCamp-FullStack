051381e2eef406331ee68d5e9efc5bd36cb2e554
Kiezmi Quinza <Kiezmi@gmail.com>
2018-11-26 12:57:41 +0100

    Funciones medias

---
+++ ejercicios-fundamentos-JS/6-functions-medium/ej6.js
@@ -0,0 +1,40 @@
+// crea la función  transformaArrayEnOtraConSusLongitudes
+// debe recibir un array y
+// devolver otro array con las longitudes de los strings recibidos en el array
+
+// Si la función no recibe un dato tipo object / array  
+// debe devolver el string 'Debo ser ejecutada con un array'
+// puedes comprobar si es un array con:
+//  typeof array === 'object' && array.length >= 0
+
+
+// Ha de pasar los test adjuntados.
+
+let transformaArrayEnOtraConSusLongitudes = (arrayDeStrings) => {
+    let arrayDeLongitudes = [];
+    // Aquí tu código.  Desde aquí:
+    if (typeof arrayDeStrings === 'object' && arrayDeStrings.length >= 0) {
+        for (let suma of arrayDeStrings) {
+            arrayDeLongitudes.push(suma.length);
+        }
+    } else {
+        return 'Debo ser ejecutada con un array';
+    }
+    // Hasta aquí.
+    return arrayDeLongitudes;
+}
+
+
+let test = require('../test.js');
+test(transformaArrayEnOtraConSusLongitudes, [
+    ['juan', 'paco', 'pepe']
+], [4, 4, 4]);
+test(transformaArrayEnOtraConSusLongitudes, [
+    ['EY', 'hoy', 'qqq']
+], [2, 3, 3]);
+test(transformaArrayEnOtraConSusLongitudes, [
+    ['EY', 'hoy', 'qqqs', 'hola']
+], [2, 3, 4, 4]);
+test(transformaArrayEnOtraConSusLongitudes, [''], 'Debo ser ejecutada con un array');
+test(transformaArrayEnOtraConSusLongitudes, [true], 'Debo ser ejecutada con un array');
+test(transformaArrayEnOtraConSusLongitudes, [{}], 'Debo ser ejecutada con un array');
---
+++ ejercicios-fundamentos-JS/6-functions-medium/ej9.js
@@ -0,0 +1,26 @@
+// crea la función extraerAcronimo
+// Esta función recibe un numero indeterminado de argumentos
+// y los recibiremos en la array input con el operador rest (...input)
+// Debe devolver un string con sus iniciales 
+// Por ejemplo:
+// extraerAcronimo('Estudiante','Del','Bootcamp','Fullstack','Geekshubs') === 'EDBFG'
+// 
+// utilizar un bucle for of
+//
+// Ha de pasar los test adjuntados.
+
+let extraerAcronimo = (...input) => {
+    let acronimo = '';
+    // Aquí tu código.  Desde aquí:
+    for (let palabra of input) {
+        acronimo += (palabra[0]); //se va sumando al string la letra 0 de cada palabra
+    }
+   // console.log(acronimo); 
+    // Hasta aquí.
+    return acronimo;
+}
+let test = require('../test.js');
+
+test(extraerAcronimo, ['Estudiante', 'Del', 'Bootcamp', 'Fullstack', 'Geekshubs'], 'EDBFG');
+test(extraerAcronimo, ['hola', 'caracola'], 'hc');
+test(extraerAcronimo, ['33', '44', ' '], '34 ');
